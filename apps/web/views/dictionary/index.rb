53f6cc2a56d0a250d01ae6b8c1ff75c93f86561d
morinoko <forby.1@gmail.com>
2019-12-16 09:29:15 +0900

    set up basic dictionary index page (with fake data for now)

---
+++ apps/web/views/dictionary/index.rb
@@ -0,0 +1,13 @@
+module Web
+  module Views
+    module Dictionary
+      class Index
+        include Web::View
+
+        # This view class is empty and doesn’t do anything special.
+        # Its only responsibility is to render a template,
+        # which is what views do by default.
+      end
+    end
+  end
+end
