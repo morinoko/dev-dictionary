dde8a0cffd1fae3531da2aa7ed24fff25a4d912c
morinoko <forby.1@gmail.com>
2019-12-17 20:33:57 +0900

    add specs for word error messages

--- spec/web/features/add_word_spec.rb
+++ spec/web/features/add_word_spec.rb
@@ -20,4 +20,20 @@ RSpec.describe 'Add a word' do
     expect(page).to have_current_path('/')
     expect(page).to have_content('New word')
   end
+
+  it 'displays list of errors when params contains errors' do
+    visit 'words/new'
+
+    within '.word-form' do
+      click_button 'Create'
+    end
+
+    expect(current_path).to eq '/words'
+
+    expect(page).to have_content('There was a problem with your submission')
+    expect(page).to have_content('English must be filled')
+    expect(page).to have_content('Japanese must be filled')
+    expect(page).to have_content('Kana must be filled')
+    expect(page).to have_content('Romaji must be filled')
+  end
 end
--- spec/web/views/words/new_spec.rb
+++ spec/web/views/words/new_spec.rb
@@ -1,10 +1,13 @@
 RSpec.describe Web::Views::Words::New, type: :view do
-  let(:exposures) { Hash[format: :html] }
+  let(:params)    { OpenStruct.new(valid?: false, error_messages: ['English must be filled', 'Japanese must be filled']) }
+  let(:exposures) { Hash[params: params] }
   let(:template)  { Hanami::View::Template.new('apps/web/templates/words/new.html.erb') }
   let(:view)      { described_class.new(template, exposures) }
   let(:rendered)  { view.render }
 
-  it 'exposes #format' do
-    expect(view.format).to eq exposures.fetch(:format)
+  it 'displays list of errors when params contains errors' do
+    expect(rendered).to include('There was a problem with your submission')
+    expect(rendered).to include('English must be filled')
+    expect(rendered).to include('Japanese must be filled')
   end
 end
